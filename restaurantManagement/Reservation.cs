564da893b909dfef179838fcb1bd096101700b59
orkhanshahbaz <113243022+orkhanshahbaz@users.noreply.github.com>
2022-12-10 14:03:06 +0100

    Restaurant Management System

---
+++ restaurantManagement/Reservation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace restaurantManagement
+{
+    public class Reservation  // Derived class
+    {
+        public Table table;
+        public int tableNumber;
+        public string guestName;
+        public DateTime arrivalTime;
+        public DateTime departureTime;
+        public int numberOfGuests;
+
+        public Reservation(int tableNumber, string guestName, DateTime arrivalTime, DateTime departureTime, int numberOfGuests)
+        {
+
+
+            this.tableNumber = tableNumber;
+            this.guestName = guestName;
+            this.arrivalTime = arrivalTime;
+            this.departureTime = departureTime;
+            this.numberOfGuests = numberOfGuests;
+
+        }
+
+        public override string ToString()
+        {
+            return "Reservation: " + tableNumber + guestName + arrivalTime + departureTime + numberOfGuests;
+        }
+
+
+
+
+    }
+}
---
+++ restaurantManagement/Table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace restaurantManagement
+{
+    public class Table  // Base class
+    {
+        public int tableNumber;
+
+        public int chairs;
+        public string location = "restaurant";
+        
+
+        public Table(int tableNumber, int chairs, string location)
+        {
+
+            this.tableNumber = tableNumber;
+            this.chairs = chairs;
+            this.location = location;
+
+        }
+
+        public override string ToString()
+        {
+            return "Number of chairs: " + chairs + "\n Location of table" + location + "\n Table number: " + tableNumber;
+
+        }
+
+    }
+}
+
